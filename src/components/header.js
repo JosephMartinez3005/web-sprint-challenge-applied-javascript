d7f7ff602dfc3ece264a199c626fbcd233d021e7
JosephMartinez3005 <52721764+JosephMartinez3005@users.noreply.github.com>
2021-01-23 22:22:29 -0800

    Finished question 1

--- src/components/header.js
+++ src/components/header.js
@@ -10,21 +10,27 @@ const Header = (title, date, temp) => {
     const currentDate = document.createElement('span');
     const h1 = document.createElement('h1');
     const tempi = document.createElement('span');
+
+
   //Adding classe to the elemets
     header.classList.add('header');
     currentDate.classList.add('date');
     tempi.classList.add('temp');
+
+
     //Adding content to the elements
     h1.textContent = title;
     currentDate.textContent = date;
-    tempi.textContent =temp;
-    //
+    tempi.textContent = temp;
+
+
+    //Createing the structure.
     header.appendChild(currentDate);
     header.appendChild(h1)
     header.appendChild(tempi)
 
       console.log(header)
-
+return header;
   //  <div class="header">
   //    <span class="date">{ date }</span>
   //    <h1>{ title }</h1>
@@ -33,8 +39,10 @@ const Header = (title, date, temp) => {
   //
 }
 
-Header()
+
 const headerAppender = (selector) => {
+  let appender = document.querySelector(`${selector}`)
+  appender.appendChild(Header())
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
